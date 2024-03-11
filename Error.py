8f774c473329d1854230e5cbaa408bb902d1fd1f
michael <mcarey1@ualberta.ca>
2024-03-10 21:39:16 -0600

    Error class doc

--- Error.py
+++ Error.py
@@ -1,3 +1,6 @@
+'''
+    General error class
+'''
 class Error:
     def __init__(self, error, details=None):
         self.error = error
@@ -8,10 +11,15 @@ class Error:
             return self.error
         return self.error + ": " + self.details
 
+'''
+    Handles unexpected tokens when doing tokenization
+'''
 class IllegalTokenError(Error):
     def __init__(self, details):
         super().__init__("Illegal character input", details)
-
+'''
+    Handles unexpected syntax in parsing
+'''
 class IllegalSyntaxError(Error):
     def __init__(self):
         super().__init__("Illegal syntax input")
