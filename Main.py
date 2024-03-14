2bdcc41540199563da0dafea0e3a6e4ba0366678
michael <careyamichael2002@gmail.com>
2024-03-13 22:56:44 -0600

    add support for passing a file

--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
 import Lexer
 import Parser
 import Interpreter
-def main():
+import sys
+
+def run_terminal():
     while True:
         line = input("mScript > ")
         if not line:
@@ -11,5 +13,20 @@ def main():
             print(str(error))
         else:
             print(Interpreter.run(res))
-
+            
+def run_file(f):
+    lines = f.read().splitlines() 
+    for line in lines:
+        res, error = Parser.run(line)
+        if error:
+            print(str(error))
+        else:
+            print(Interpreter.run(res))
+            
+def main():
+    if len(sys.argv) > 1 and sys.argv[1][len(sys.argv[1]) - 3:len(sys.argv[1])] == ".ms":
+        with open(sys.argv[1], "r") as f:
+            run_file(f)
+    else:
+        run_terminal()
 main()
